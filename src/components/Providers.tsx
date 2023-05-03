bb53f8c30baf4e62da650edf0aa8e4be0f5a0084
ismum <ichowdhury4@fordham.edu>
2023-05-03 16:37:16 -0400

    created large heading ui component and create new file for navbar

---
+++ src/components/Providers.tsx
@@ -0,0 +1,11 @@
+import { ReactNode, FC } from 'react'
+
+interface ProvidersProps {
+  children: ReactNode
+}
+
+const Providers = ({ children} : ProvidersProps) => {
+  return <div>Providers</div>
+}
+
+export default Providers
