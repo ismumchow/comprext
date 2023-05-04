7981aeaa53e54c93796e3203f1c195775ad02226
ismum <ichowdhury4@fordham.edu>
2023-05-04 11:26:26 -0400

    provider component interface created and props passed

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,21 @@
-import { ReactNode, FC } from 'react'
+'use client'
+
+import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider } from 'next-themes'
+import type { FC, ReactNode } from 'react'
 
 interface ProvidersProps {
   children: ReactNode
 }
+// because its FC, the function will return a react node
+// because we pass ProviderProps, we know that the props will have children of type ReactNode
 
-const Providers = ({ children} : ProvidersProps) => {
-  return <div>Providers</div>
+const Providers: FC<ProvidersProps> = ({ children }) => {
+  return (
+    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+      <SessionProvider>{children}</SessionProvider>
+    </ThemeProvider>
+  )
 }
 
 export default Providers
