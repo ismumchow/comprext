38a40cefa13b198c3a3e25ef25308a47d251c592
ismum <ichowdhury4@fordham.edu>
2023-05-05 11:47:21 -0400

    homepage finished with typewriter and main text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,33 @@
-const { fontFamily } = require('tailwindcss/defaultTheme');
-const { colors } = require('tailwindcss/colors');
+const { colors } = require('tailwindcss/colors')
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  content: ["./src/**/*.{html,js,ts,tsx}"],
+  darkMode: ['class'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    container:{
-      center:true,
-      padding:'1.5rem',
+    container: {
+      center: true,
+      padding: '1.5rem',
       screens: {
-        '2xl': '1360'
-      }
+        '2xl': '1360px',
+      },
     },
     extend: {
-      fontFamily: {
-        sans: ['var(--font-inter)', ...fontFamily.sans],
-     },
-     colors: {
-      ...colors, 
-      'light-gold' : '#f5bc51',
-      'dark-gold'  :  '#533519',
-
-     }
-    }
+ 
+      colors: {
+        ...colors,
+        'light-gold': '#f5bc51',
+        'dark-gold': '#533519',
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss-animate'),
-    require('@tailwindcss/typography')
-  ],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 }
