27da1e56de14d0b2af94d48f355771fface0d975
ismum <ichowdhury4@fordham.edu>
2023-05-05 11:27:42 -0400

    created toast the main layout and finished toast on buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import { getServerSession } from 'next-auth'
 import Link from 'next/link'
 import  ThemeToggle  from '@/components/ThemeToggle'
-import { buttonVariants } from './ui/Button'
+import { buttonVariants } from '@/ui/Button'
 import SignInButton from '@/components/ui/SignInButton'
 import SignOutButton from '@/components/ui/SignOutButton'
 
-const Navbar = async () => {
+const Navbar  = async () => {
   const session = await getServerSession()
   return (
     <div className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+const { colors } = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
-    extend: {},
+    container:{
+      center:true,
+      padding:'1.5rem',
+      screens: {
+        '2xl': '1360'
+      }
+    },
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+     },
+     colors: {
+      ...colors, 
+      'light-gold' : '#f5bc51',
+      'dark-gold'  :  '#533519',
+
+     }
+    }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss-animate'),
+    require('@tailwindcss/typography')
+  ],
 }
